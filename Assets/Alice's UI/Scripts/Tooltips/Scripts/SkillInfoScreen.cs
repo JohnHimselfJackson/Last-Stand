2ec6a8c8cd046ee9fca7b39089e6c3855e745ed9
Alice Shinnick <61133327+VulpineAlice@users.noreply.github.com>
2020-11-16 07:59:20 +0800

    Main Menu Repaired and Implemented

--- Assets/Alice's UI/Scripts/Tooltips/Scripts/SkillInfoScreen.cs
+++ Assets/Alice's UI/Scripts/Tooltips/Scripts/SkillInfoScreen.cs
@@ -1,27 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class SkillInfoScreen : MonoBehaviour
 {
-
-    public TMP_Text skillN, skillD;
-
     [SerializeField]
     private Color armorColour, weaponColour;
 
-    private GameObject
+    [SerializeField]
+    private GameObject weaponTree, armorTree, armorSwap, weaponSwap, armorIcons, weaponIcons;
 
-    // Start is called before the first frame update
-    void Start()
+    public Image skillNameC, selectedSkill, skillDescriptionC;
+
+    public TMP_Text skillN, skillD;
+
+    //Display Selected Skill Info
+    //Name
+    //Image
+    //Description
+
+    public void SwapTree(int pageNum)
     {
-        
+        switch(pageNum)
+        {
+            case 0:
+                {
+                    skillNameC.color = armorColour;
+                    skillDescriptionC.color = armorColour;
+
+                    armorSwap.SetActive(false);
+                    weaponSwap.SetActive(true);
+
+                    armorTree.SetActive(true);
+                    weaponTree.SetActive(false);
+
+                    armorIcons.SetActive(true);
+                    weaponIcons.SetActive(false);
+
+                    return;
+                }
+            case 1:
+                {
+                    skillNameC.color = weaponColour;
+                    skillDescriptionC.color = weaponColour;
+
+                    armorSwap.SetActive(true);
+                    weaponSwap.SetActive(false);
+
+                    armorTree.SetActive(false);
+                    weaponTree.SetActive(true);
+
+                    armorIcons.SetActive(false);
+                    weaponIcons.SetActive(true);
+
+                    return;
+                }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SceneChange(string sceneName)
     {
-        
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }
