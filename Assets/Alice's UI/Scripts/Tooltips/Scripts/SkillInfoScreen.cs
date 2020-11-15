815e01fbd4e1f0744691d5c5b2159f2b70a68616
Alice Shinnick <61133327+VulpineAlice@users.noreply.github.com>
2020-11-15 10:02:55 +0800

    Working on Info Page Setup

---
+++ Assets/Alice's UI/Scripts/Tooltips/Scripts/SkillInfoScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SkillInfoScreen : MonoBehaviour
+{
+
+    public TMP_Text skillN, skillD;
+
+    [SerializeField]
+    private Color armorColour, weaponColour;
+
+    private GameObject
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
